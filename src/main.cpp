4ecb4c280380a64295c0a43604f38286db551d91
Jackson Seal <sealj553@gmail.com>
2018-09-23 21:38:35 -0400

    cleanups, added green detection

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 #include <opencv2/video/video.hpp>
 #include <iostream>
 #include <vector>
+#include <string>
 #include <chrono>
 
 using std::cout;
 using std::endl;
+using std::string;
 using std::vector;
 using std::stringstream;
 using namespace std::chrono;
@@ -40,8 +42,21 @@ Scalar gMax(60 + gSensitivity, 255, 255);
 
 namespace B {
     vector<vector<Point>> contours;
+    vector<double> contourAreas;
     vector<Vec4i> hierarchy;
     vector<vector<Point>> polygons;
+    vector<Rect> boundRect;
+    vector<Point> centers;
+
+    Mat orig;
+    Mat thresh;
+    Mat canny_output;
+};
+
+namespace G {
+    vector<vector<Point>> contours;
+    vector<double> contourAreas;
+    vector<Vec4i> hierarchy;
     vector<Rect> boundRect;
     vector<Point> centers;
 
@@ -74,8 +89,8 @@ int main(){
 
 #ifndef USE_WEBCAM
     frame = imread(test_image);
-    cout << "\nHit 'q' to exit...\n";
 #else
+    cout << "\nHit 'q' to exit...\n";
     while(waitKey(1) != 'q'){
         cap >> frame;
 #endif
@@ -111,14 +126,19 @@ void prepFrame2(Mat &frame, const Scalar &minColorRange, const Scalar &maxColorR
 void processFrame(const Mat &frame){
     Mat processed = frame.clone();
     prepFrame(processed);
+
     processB(processed, frame);
     drawB();
+
+    processG(processed, frame);
+    drawG();
 }
 
 void processB(const Mat &_frame, const Mat &_orig){
     using namespace B;
 
     contours.clear();
+    contourAreas.clear();
     hierarchy.clear();
     polygons.clear();
     boundRect.clear();
@@ -137,9 +157,13 @@ void processB(const Mat &_frame, const Mat &_orig){
     findContours(canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
     //CV_RETR_EXTERNAL = get just external contours (no nesting)
 
-    //get polygons from contours
     polygons.resize(contours.size());
+    contourAreas.resize(contours.size());
     for(unsigned i = 0; i < contours.size(); ++i){
+        //get contour areas
+        contourAreas[i] = contourArea(contours[i]); 
+
+        //get polygons from contours
         approxPolyDP(contours[i], polygons[i], polyEpsilon, true);
     }
 
@@ -155,7 +179,6 @@ void processB(const Mat &_frame, const Mat &_orig){
         boundRect[i] = boundingRect(polygons[i]);
         //find center of bounding rectangle
         centers[i] = Point(boundRect[i].x + boundRect[i].width/2, boundRect[i].y + boundRect[i].height/2);
-
     }
 }
 
@@ -164,9 +187,11 @@ void drawB(){
 
     Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
 
+    for(unsigned i = 0; i < contours.size(); ++i){
+        drawContours(drawing, contours, i, Scalar(32,32,32),   2, 8, hierarchy, 0, Point());
+    }
+
     for(unsigned i = 0; i < polygons.size(); ++i){
-        //draw stuff
-        drawContours(drawing, contours, i, Scalar(32,32,32), 2, 8, hierarchy, 0, Point());
         drawContours(drawing, polygons, i, Scalar(32,255,255), 1, 8, hierarchy, 0, Point());
         drawContours(orig,    polygons, i, Scalar(32,255,255), 1, 8, hierarchy, 0, Point());
 
@@ -174,8 +199,77 @@ void drawB(){
         circle(orig,    centers[i], 3, Scalar(32,255,255));
     }
 
+    for(unsigned i = 0; i < contours.size(); ++i){
+        string str("A=");
+        str += std::to_string(contourAreas[i]);
+        putText(orig, str, centers[i], FONT_HERSHEY_PLAIN, 1, Scalar(255,255,255));
+    }
+
     //imshow("canny", canny_output);
-    imshow("threshold", thresh);
-    imshow("drawing", drawing);
-    imshow("orig", orig);
+    imshow("bthreshold", thresh);
+    imshow("bdrawing", drawing);
+    imshow("borig", orig);
+}
+
+void processG(const Mat &_frame, const Mat &_orig){
+    using namespace G;
+
+    contours.clear();
+    contourAreas.clear();
+    hierarchy.clear();
+    boundRect.clear();
+    centers.clear();
+    //
+
+    orig = _orig.clone();
+    thresh = _frame.clone();
+
+    prepFrame2(thresh, gMin, gMax);
+
+    //canny edge detection
+    Canny(thresh, canny_output, edgeThresh, maxEdgeThresh, 3);
+
+    //find contours
+    findContours(canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    //CV_RETR_EXTERNAL = get just external contours (no nesting)
+
+    boundRect.reserve(contours.size());
+    centers.reserve(contours.size());
+
+    contourAreas.resize(contours.size());
+    for(unsigned i = 0; i < contours.size(); ++i){
+        //get contour areas
+        contourAreas[i] = contourArea(contours[i]); 
+        //get bounding rectangle for each contour 
+        boundRect[i] = boundingRect(contours[i]);
+        //find center of bounding rectangle
+        centers[i] = Point(boundRect[i].x + boundRect[i].width/2, boundRect[i].y + boundRect[i].height/2);
+    }
+}
+
+void drawG(){
+    using namespace G;
+
+    Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+
+    for(unsigned i = 0; i < contours.size(); ++i){
+        //draw stuff
+        drawContours(drawing, contours, i, Scalar(32,32,32), 2, 8, hierarchy, 0, Point());
+        drawContours(drawing, contours, i, Scalar(32,255,255), 1, 8, hierarchy, 0, Point());
+        drawContours(orig,    contours, i, Scalar(32,255,255), 1, 8, hierarchy, 0, Point());
+
+        circle(drawing, centers[i], 3, Scalar(32,255,255));
+        circle(orig,    centers[i], 3, Scalar(32,255,255));
+    }
+
+    for(unsigned i = 0; i < contours.size(); ++i){
+        string str("A=");
+        str += std::to_string(contourAreas[i]);
+        putText(orig, str, centers[i], FONT_HERSHEY_PLAIN, 1, Scalar(255,255,255));
+    }
+
+    //imshow("canny", canny_output);
+    imshow("gthreshold", thresh);
+    imshow("gdrawing", drawing);
+    imshow("gorig", orig);
 }
